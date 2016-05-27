9b1dae0dfaa01d7f5aa4d4edf1f48d0ce22ff2f3
Mistermaru <mistermaru_@live.nl>
2016-05-27 13:48:46 +0200

    Added AddPanel to the application.

---
+++ ExampleJavaDBApp/src/javadbkoppelingws2/AddPanel.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javadbkoppelingws2;
+
+/**
+ *
+ * @author Maru
+ */
+public class AddPanel extends Tab  {
+
+    public AddPanel(int width, int height) {
+        super(width, height);
+    }
+    
+    
+}
