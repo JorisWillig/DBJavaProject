de967de51004be15b2b8ed7f644308605780fb36
stefmann <stefvandegraaf@msn.com>
2016-06-01 13:07:17 +0200

    almost done with overview

--- ExampleJavaDBApp/src/javadbkoppelingws2/OverviewPanel.java
+++ ExampleJavaDBApp/src/javadbkoppelingws2/OverviewPanel.java
@@ -7,7 +7,13 @@ package javadbkoppelingws2;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import static javadbkoppelingws2.Tab.conn;
 import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 /**
  *
@@ -50,6 +56,10 @@ public class OverviewPanel extends Tab {
 
         ButtonAction action;
         MyTableModel model;
+        JTable table;
+        String[][] dataValues = {};
+        String[] columnNames = {};
+        JScrollPane rightPanel;
 
         public ButtonListener(ButtonAction action) {
             this.action = action;
@@ -57,16 +67,137 @@ public class OverviewPanel extends Tab {
 
         public void actionPerformed(ActionEvent e) {
             if (action == ButtonAction.Overzict_op_land) {
-
+ 
             }
             if (action == ButtonAction.Overzicht_op_traject) {
+                String query = " SELECT Student_Traject.traject_id, Traject.naam"
+                        + ", count(Student_Traject.Student_id) as Aantal_studenten "
+                        + "FROM Student_Traject "
+                        + "LEFT JOIN Traject ON Student_Traject.traject_id = Traject.traject_id "
+                        + "GROUP BY Student_Traject.traject_id ORDER BY Aantal_studenten DESC;";
 
+                ResultSet res = doQuery(query);
+
+                columnNames = new String[3];
+                columnNames[0] = "Traject ID";
+                columnNames[1] = "Traject naam";
+                columnNames[2] = "Aantal studenten";
+
+                int rowCount = 0;
+                try {
+                    System.out.println("After try");
+                    if (res.last()) {
+                        rowCount = res.getRow();
+                        System.out.println(rowCount);
+                        res.beforeFirst();
+                    }
+                    System.out.println("After row count set");
+                    dataValues = new String[rowCount][columnNames.length];
+                    int counter = 0;
+                    System.out.println("Before while");
+                    while (res.next()) {
+                        System.out.println("begin while");
+                        dataValues[counter][0] = res.getString("Student_Traject.traject_id");
+                        dataValues[counter][1] = res.getString("Traject.naam");
+                        dataValues[counter][2] = res.getString("Aantal_studenten");
+                        counter++;
+                        System.out.println("while " + counter);
+                    }
+                    System.out.println("After while");
+                    System.out.println(table);
+                    System.out.println(rightPanel);
+                    if (table == null || rightPanel == null) {
+                        System.out.println("fsgs");
+                        table = new JTable(new MyTableModel(dataValues, columnNames));
+
+                        model = (MyTableModel) table.getModel();
+
+                        table.setShowVerticalLines(false);
+                        table.setRowSelectionAllowed(true);
+                        table.setColumnSelectionAllowed(false);
+
+                        rightPanel = new JScrollPane(table);
+                        rightPanel.setSize(WIDTH / 2 - X_MARGIN * 2, HEIGHT - Y_MARGIN * 6 - COMPONENT_HEIGHT);
+                        rightPanel.setLocation(WIDTH / 2 + X_MARGIN, Y_MARGIN);
+                        add(rightPanel);
+
+                    }
+
+                    model.setColumnNames(columnNames);
+                    model.setNewData(dataValues);
+                    repaint();
+                } catch (SQLException e2) {
+                    //TODO
+                }
             }
+
             if (action == ButtonAction.Overzicht_op_school) {
+
+                String query = "select School.school_id, School.schoolnaam"
+                        + ", count(Exchange_Student.student_id) as Aantal_studenten "
+                        + "FROM School "
+                        + "LEFT JOIN Exchange_Student ON Exchange_Student.school_id = School.school_id "
+                        + "GROUP BY School.school_id ORDER BY Aantal_studenten DESC;";
+                ResultSet res = doQuery(query);
+
+                columnNames = new String[3];
+                columnNames[0] = "School ID";
+                columnNames[1] = "School naam";
+                columnNames[2] = "Aantal studenten";
+                int rowCount = 0;
+                try {
+                    if (res.last()) {
+                        rowCount = res.getRow();
+                        System.out.println(rowCount);
+                        res.beforeFirst();
+                    }
+                    dataValues = new String[rowCount][columnNames.length];
+                    int counter = 0;
+                    while (res.next()) {
+                        dataValues[counter][0] = res.getString("School.school_id");
+                        dataValues[counter][1] = res.getString("School.schoolnaam");
+                        dataValues[counter][2] = res.getString("Aantal_studenten");
+                        counter++;
+                    }
+                    if (table == null || rightPanel == null) {
+                        table = new JTable(new MyTableModel(dataValues, columnNames));
+
+                        model = (MyTableModel) table.getModel();
+
+                        table.setShowVerticalLines(false);
+                        table.setRowSelectionAllowed(true);
+                        table.setColumnSelectionAllowed(false);
+
+                        rightPanel = new JScrollPane(table);
+                        rightPanel.setSize(WIDTH / 2 - X_MARGIN * 2, HEIGHT - Y_MARGIN * 6 - COMPONENT_HEIGHT);
+                        rightPanel.setLocation(WIDTH / 2 + X_MARGIN, Y_MARGIN);
+                        add(rightPanel);
+
+                    }
+
+                    model.setColumnNames(columnNames);
+                    model.setNewData(dataValues);
+                    repaint();
+                } catch (SQLException e2) {
+                    //TODO
+                }
 
             }
 
         }
 
+        public ResultSet doQuery(String query) {
+            Statement stat;
+            ResultSet res = null;
+            try {
+                stat = conn.createStatement();
+
+                res = stat.executeQuery(query);
+                int counter = 0;
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+            return res;
+        }
     }
 }
